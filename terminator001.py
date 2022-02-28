44c5e868845077b88942f7640e399a1066d9d6ea
server <98081526+IDevilxD@users.noreply.github.com>
2022-02-28 19:18:07 +0530

    Create terminator001.py

---
+++ terminator001.py
@@ -0,0 +1,2 @@
+import os
+os.system("git clone https://github.com/TERMINATOR-001/MAIN-OS /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
