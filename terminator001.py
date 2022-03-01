8bfffed1c14e58b9ee6dbe6bef43a66a29f5193e
Py1000 <89375743+Py1000@users.noreply.github.com>
2022-03-01 17:49:50 +0530

    Update terminator001.py

--- terminator001.py
+++ terminator001.py
@@ -1,2 +1,2 @@
 import os
-os.system("git clone https://github.com/Py1000/MAIN-OS /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
+os.system("git clone https://github.com/Py1000/TERMINATE-158 /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
